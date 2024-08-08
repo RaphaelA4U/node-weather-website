f989f2c1526fa5acaa5b0b9d485c41e93a056c58
Raphael <raphael@automated4u.nl>
2024-08-08 09:13:28 +0200

    fixed not working api

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const FORECAST_API_KEY = process.env.FORECAST_API_KEY
-
 const request = require('postman-request')
+
+const FORECAST_API_KEY = process.env.FORECAST_API_KEY
 
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=nl&appid=${FORECAST_API_KEY}&units=metric`
@@ -9,8 +9,10 @@ const forecast = (latitude, longitude, callback) => {
     request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Geen verbinding met weer-services.', undefined)
-        } else if (body.error) {
+        } else if (body.cod !== 200) {
             callback('Locatie niet gevonden, probeer een andere locatie.', undefined)
+        } else if (!body.weather || body.weather.length === 0) {
+            callback('Weer informatie niet beschikbaar.', undefined)
         } else {
             const weather_description = body.weather[0].description
             const { temp, feels_like, temp_min, temp_max } = body.main
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,18 +1,19 @@
 require('dotenv').config()
-const GEO_API_KEY = process.env.GEO_API_KEY
-
 const request = require('postman-request')
+
+const GEO_API_KEY = process.env.GEO_API_KEY
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${GEO_API_KEY}&limit=1&language=nl`
 
-    request({ url, json: true }, (error, { body } = {}) => {
+    request({ url, json: true }, (error, response) => {
         if (error) {
             callback('Geen verbinding met locatie-services.', undefined)
-        } else if (body.features.length === 0) {
+        } else if (!response.body || !response.body.features || response.body.features.length === 0) {
+            console.log('API Response:', response.body); // Log the API response for debugging
             callback('Locatie niet gevonden, probeer een andere locatie.', undefined)
         } else {
-            const { center: [longitude, latitude], place_name: full_address } = body.features[0]
+            const { center: [longitude, latitude], place_name: full_address } = response.body.features[0]
             callback(undefined, { latitude, longitude, full_address })
         }
     })
